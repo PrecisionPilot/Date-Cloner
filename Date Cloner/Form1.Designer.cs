f1b3295ef0a400aff352423ce46942b70fa1bed8
Sean Wang <seanwaterloo2997@gmail.com>
2024-02-20 21:28:53 -0500

    Initial commit

---
+++ Date Cloner/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Date_Cloner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Copy = new System.Windows.Forms.OpenFileDialog();
+            this.Directory = new System.Windows.Forms.Label();
+            this.beginButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.Instructions = new System.Windows.Forms.Label();
+            this.RenameButton = new System.Windows.Forms.Button();
+            this.RenameFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Copy
+            // 
+            this.Copy.Filter = "VLC Converted Files | *-converted.mp4";
+            this.Copy.Multiselect = true;
+            this.Copy.FileOk += new System.ComponentModel.CancelEventHandler(this.Copy_FileOk);
+            // 
+            // Directory
+            // 
+            this.Directory.AutoSize = true;
+            this.Directory.Location = new System.Drawing.Point(163, 94);
+            this.Directory.Name = "Directory";
+            this.Directory.Size = new System.Drawing.Size(0, 13);
+            this.Directory.TabIndex = 4;
+            // 
+            // beginButton
+            // 
+            this.beginButton.Location = new System.Drawing.Point(342, 35);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(75, 23);
+            this.beginButton.TabIndex = 5;
+            this.beginButton.Text = "Begin!";
+            this.beginButton.UseVisualStyleBackColor = true;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 123);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(405, 23);
+            this.progressBar.TabIndex = 6;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // Instructions
+            // 
+            this.Instructions.AutoSize = true;
+            this.Instructions.Location = new System.Drawing.Point(9, 9);
+            this.Instructions.Name = "Instructions";
+            this.Instructions.Size = new System.Drawing.Size(406, 13);
+            this.Instructions.TabIndex = 7;
+            this.Instructions.Text = "Select files you want to copy and paste and make sure they have the same file nam" +
+    "e";
+            // 
+            // RenameButton
+            // 
+            this.RenameButton.Location = new System.Drawing.Point(12, 68);
+            this.RenameButton.Name = "RenameButton";
+            this.RenameButton.Size = new System.Drawing.Size(405, 23);
+            this.RenameButton.TabIndex = 9;
+            this.RenameButton.Text = "Remove the \'-converted\' an MP4 file";
+            this.RenameButton.UseVisualStyleBackColor = true;
+            this.RenameButton.Click += new System.EventHandler(this.RenameButton_Click);
+            // 
+            // RenameFileDialog
+            // 
+            this.RenameFileDialog.Filter = "Video | *.mp4";
+            this.RenameFileDialog.Multiselect = true;
+            this.RenameFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.RenameFileDialog_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clone/Replace VLC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(429, 158);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RenameButton);
+            this.Controls.Add(this.Instructions);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.Directory);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(445, 242);
+            this.Name = "Form1";
+            this.Text = "Date Cloner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog Copy;
+        private System.Windows.Forms.Label Directory;
+        private System.Windows.Forms.Button beginButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label Instructions;
+        private System.Windows.Forms.Button RenameButton;
+        private System.Windows.Forms.OpenFileDialog RenameFileDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Date Cloner/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Date_Cloner
+{
+    public partial class Form1 : Form
+    {
+        string exception = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Copy_FileOk(object sender, CancelEventArgs e)
+        {
+            label1.Text = Copy.FileNames.Length + " files selected";
+        }
+
+        private async void beginButton_Click(object sender, EventArgs e)
+        {
+            if (Copy.FileName == "")// if no files were selected
+            {
+                MessageBox.Show("You have not made a selection", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                button1.Enabled = false;
+                beginButton.Enabled = false;
+                RenameButton.Enabled = false;
+
+                var progress = new Progress<int>(v =>
+                {
+                    progressBar.Value = v;
+                });
+
+
+                Task t = Task.Run(() => Clone(progress));
+                await Task.WhenAll(t);
+            }
+            catch (Exception exception)//show an error message box, incase if somthing goes wrong for good measure
+            {
+                MessageBox.Show(exception.ToString(), "Something Went Wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Copy.FileName = "";//clear the OpenFileDialog for 'Copy'
+            label1.Text = "";
+            progressBar.Value = 0;
+
+            button1.Enabled = true;
+            beginButton.Enabled = true;
+            RenameButton.Enabled = true;
+        }
+
+        public void Clone(IProgress<int> progress)//executed in another thread
+        {
+            int copiedFiles = 0;
+            List<string> toCopyFiles = new List<string>();
+
+            for (int I = 0; I < Copy.FileNames.Length; I++)//for each files to copy
+            {
+                for (int i = 0; i < Copy.FileNames.Length; i++)//find a matching file name from the files to paste
+                {
+                    string toCopyFile = Copy.FileNames[I].Substring(0, Copy.FileNames[I].Length - 14) +".mp4";//store the file that are to be copied as a variable
+
+                    if (Path.GetFileNameWithoutExtension(toCopyFile) + "-converted.mp4" == Path.GetFileName(Copy.FileNames[i]))
+                    {
+                        try
+                        {
+                            File.SetCreationTime(Copy.FileNames[i], File.GetCreationTime(toCopyFile));
+                            File.SetLastWriteTime(Copy.FileNames[i], File.GetLastWriteTime(toCopyFile));
+                            toCopyFiles.Add(toCopyFile);//add the file to the list
+                            copiedFiles++;
+                            break;
+                        }
+                        catch(Exception e)
+                        {
+                            exception = "\n\n" + e.ToString();
+                        }
+                    }
+                }
+                progress.Report(copiedFiles * 80 / Copy.FileNames.Length);
+            }
+            string[] toCopyFilesArray = toCopyFiles.ToArray();
+            
+            foreach (string file in toCopyFilesArray)//Delete original files
+            {
+                Console.WriteLine(file);
+                File.Delete(file);
+            }
+
+            //to rename the converted files
+            bool success = true;
+            string unsuccessfulFilenames = "";
+            int unsuccessfulFiles = 0;
+            int renamedFiles = 0;
+            foreach (string fileName in Copy.FileNames)
+            {
+                try
+                {
+                    File.Move(fileName, fileName.Substring(0, fileName.Length - 14) + ".mp4");
+                    renamedFiles++;
+                    progress.Report(80 + (renamedFiles * 20 / Copy.FileNames.Length));
+                }
+                catch
+                {
+                    success = false;
+                    unsuccessfulFilenames += Path.GetFileName(fileName) + "\n";
+                    unsuccessfulFiles++;
+                }
+            }
+
+            if (copiedFiles > 0)
+            {
+                if (success)
+                    MessageBox.Show("Successfully copied/pasted date from " + copiedFiles + " file(s)", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Successfully copied/pasted date from " + copiedFiles + " file(s)\n\n" +
+                        "Failed to rename:\n" + unsuccessfulFilenames + exception, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else//if no file dates were copied/pasted
+                MessageBox.Show("No action has been done" + exception, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            exception = "";
+        }
+
+        void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Copy.ShowDialog();
+        }
+
+        private void Paste_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void RenameButton_Click(object sender, EventArgs e)
+        {
+            RenameFileDialog.ShowDialog();
+        }
+        private void RenameFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Are you sure you remove the '-converted' from " + RenameFileDialog.FileNames.Length +
+                " file(s)\nthis action cannot be undone with this software", "Conformation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
+            {
+                bool success = true;
+                string unsuccessfulFiles = "";
+                int renamedFiles = 0;
+                foreach (string fileName in RenameFileDialog.FileNames)
+                {
+                    try
+                    {
+                        File.Move(fileName, fileName.Substring(0, fileName.Length - 14) + ".mp4");
+                        renamedFiles++;
+                        progressBar.Value = renamedFiles * 100 / RenameFileDialog.FileNames.Length;
+                    }
+                    catch
+                    {
+                        success = false;
+                        unsuccessfulFiles += Path.GetFileName(fileName) + "\n";
+                    }
+                }
+                RenameFileDialog.FileName = "";
+                if (success)
+                    MessageBox.Show("Successfully renamed " + renamedFiles + " file(s)", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                {
+                    MessageBox.Show("Successfully renamed " + renamedFiles + " file(s)\n\nFailed to rename:\n" + unsuccessfulFiles,
+                        "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                progressBar.Value = 0;
+            }
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
